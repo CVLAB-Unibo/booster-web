ca7132294c539227d94cc16ee4b61fb8a36c82d6
pierlui92 <zamapierluigi@hotmail.it>
2022-04-14 12:08:56 +0200

    update tables

--- _includes/tables/scripts/create_tables.py
+++ _includes/tables/scripts/create_tables.py
@@ -1,12 +1,12 @@
 import os
 
-template = open("../template.html").readlines()
+template = open("template.html").readlines()
 template = "".join(template)
 
 for setup in ["balanced", "unbalanced"]:
     for split in ["test"]:
         for occ in ["all", "noocc"]:
-            for res in ["full", "quarter"]:
+            for res in ["full", "half", "quarter"]:
                 for c in ["c0", "c1", "c2", "c3", "all"]:
                     table = setup + "_" +split + "_" + occ + "_" + res + "_" + c
                     with open("../" + table + ".html", "w") as fout:
