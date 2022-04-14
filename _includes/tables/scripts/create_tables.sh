0466b43d7420e4a6390c4b90d2f6d00b118d86de
pierlui92 <zamapierluigi@hotmail.it>
2022-04-14 10:14:40 +0200

    update tables

--- _includes/tables/scripts/create_tables.sh
+++ _includes/tables/scripts/create_tables.sh
@@ -4,12 +4,12 @@ do
     do
         for occ in all noocc
         do
-            for res in full quarter
+            for res in full quarter half
             do
                 for cls in c0 c1 c2 c3 all
                 do
                     echo "{% include tables/"$setup"_"$split"_"$occ"_"$res"_"$cls".html %}"
-                    # cp ../template.html "../"$setup"_"$split"_"$occ"_"$res"_"$cls".html"
+                    # cp template.html "../"$setup"_"$split"_"$occ"_"$res"_"$cls".html"
                 done
             done
         done
